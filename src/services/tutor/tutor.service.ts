d93dd5687eb13dcfe41653b3fce61a0b6c61208e
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2026-02-25 20:35:34 +0600

    added tutor service

---
+++ src/services/tutor/tutor.service.ts
@@ -0,0 +1,26 @@
+type ITutorParams = {
+  search: string;
+  categoryId: string;
+  price: string;
+  rating: string;
+};
+
+export const getTutors = async (params: ITutorParams) => {
+  const query = new URLSearchParams(params).toString();
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/users?${query}`,
+    {
+      cache: "no-store",
+    },
+  );
+  const data = await res.json();
+  console.log({ data });
+  return data.data;
+};
+
+export const getCategories = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
+  const data = await res.json();
+  return data.data;
+};
